678b786ad9e3c8b9d1a721f6ce255e9fec5a3629
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-03 18:44:18 -0700

    add NavLinks

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">KICKBALL LEAGUE</header>
-
       <BrowserRouter>
+        <header className="App-header">KICKBALL LEAGUE</header>
+
+        <NavLink to="/">Home Page</NavLink>
+        <NavLink to="/teams">Teams</NavLink>
+        <NavLink to="/players">Players</NavLink>
+
         <Switch>
           <Route path="/teams" />
           <Route path="/players" />
